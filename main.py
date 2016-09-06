19fb420d54ea177eff91749f348c0aaa26b09591
Uroboroz <evgeniumakov97@gmail.com>
2016-09-06 20:22:06 +0300

    Version v2

--- main.py
+++ main.py
@@ -2,48 +2,49 @@
 import random
 
 '''
-
 1. Подстановка
 2. Перестановка
 3. Гамирование (шифр Виженера)
-
 '''
+
 
 class Substitution_cipher():
     key_map = {}
-    abc = {}
 
     @staticmethod
     def random_list(list_):
         ret = []
         while len(list_) > 0:
             ret.append(random.choice(list_))
-            list_.remove(ret[len(ret)-1])
+            list_.remove(ret[len(ret) - 1])
         return ret
 
-    def key_generator(self, data):
+    def key_generator1(self, data):
         key_, val_ = [], []
         for s in data:
-            self.abc[s] = s
             key_.append(s)
             val_.append(s)
 
         self.key_map = dict(zip(self.random_list(key_), self.random_list(val_)))
 
+    def key_generator2(self):
+        self.key_map = dict(zip(self.random_list(list(range(256))), self.random_list(list(range(256)))))
+
     def encrypt(self, data):
-        self.key_generator(data)
-        encrypt_data = ""
+        self.key_generator2()
+        encrypt_data = b""
         for s in data:
-            encrypt_data += self.key_map[s]
+            encrypt_data += bytes([self.key_map[s]])
         return encrypt_data
 
     def decrypt(self, data, key_map):
         self.key_map = key_map
-        decrypt_data = ""
+        decrypt_data = b""
         key_reverse = {v: k for k, v in self.key_map.items()}
         for s in data:
-            decrypt_data += key_reverse[s]
+            decrypt_data += bytes([key_reverse[s]])
         return decrypt_data
+
 
 class Transposition_cipher():
     key_map = {}
@@ -54,50 +55,51 @@ class Transposition_cipher():
         ret = []
         while len(list_) > 0:
             ret.append(random.choice(list_))
-            list_.remove(ret[len(ret)-1])
+            list_.remove(ret[len(ret) - 1])
         return ret
 
     def key_generator(self, data):
-        self.i = int(random.uniform(5, len(data)-1))
+        self.i = int(random.uniform(5, len(data) - 1))
         self.key_map = dict(zip(self.random_list(list(range(self.i))), self.random_list(list(range(self.i)))))
 
     def encrypt(self, data):
         self.key_generator(data)
-        encrypt_data = ""
+        encrypt_data = b""
         if len(data) % self.i != 0:
-            data += " "*(self.i - len(data) % self.i)
+            data += b" " * (self.i - len(data) % self.i)
         for s in range(0, len(data), self.i):
-            temp = ""
+            temp = b""
             for i in range(self.i):
-                temp += data[s+self.key_map[i]]
+                temp += bytes([data[s + self.key_map[i]]])
             encrypt_data += temp
         return encrypt_data
 
     def decrypt(self, data, key_map):
         self.key_map = key_map
-        decrypt_data = ""
+        decrypt_data = b""
         key_reverse = {v: k for k, v in self.key_map.items()}
         for s in range(0, len(data), self.i):
-            temp = ""
+            temp = b""
             for i in range(self.i):
-                temp += data[s+key_reverse[i]]
+                temp += bytes([data[s + key_reverse[i]]])
             decrypt_data += temp
-        return decrypt_data.strip()
+        return decrypt_data[:len(decrypt_data) - self.i:]
+
 
 class Vigenere_cipher():
-    key_string = ""
+    key_string = b""
 
     def xor_strings(self, xs, ys):
         if len(xs) % len(self.key_string) != 0:
-            xs += " "*(len(self.key_string) - len(xs) % len(self.key_string))
-        result = ""
+            xs += b" " * (len(self.key_string) - len(xs) % len(self.key_string))
+        result = b""
         for i in range(len(ys)):
-            result += chr(ord(xs[i]) ^ ord(ys[i]))
+            result += bytes([xs[i] ^ ys[i]])
         return result
 
     def key_generator(self, data):
-        self.key_string += "".join([chr(int(random.uniform(0, 256))) for i in range(int(random.uniform(2, len(data))))])
-        print(self.key_string)
+        self.key_string = b"".join(
+                [bytes([int(random.uniform(0, 256))]) for i in range(int(random.uniform(2, len(data))))])
         return self.key_string
 
     def encrypt(self, data):
@@ -107,10 +109,8 @@ class Vigenere_cipher():
         self.key_string = key_string
         return self.xor_strings(data, self.key_string).strip()
 
-def main():
-    name = "./data.txt"
-    data = open(name).read()
 
+def Test_Substitution(name, data):
     ###########################################
     #                                         #
     #           SUBSTITUTION CIPHER           #
@@ -120,19 +120,22 @@ def main():
     sub_ciph = Substitution_cipher()
     print("Data    :", data)
     print("Encrypt :", sub_ciph.encrypt(data))
-    open(name+".sub.enc", "w").write(sub_ciph.encrypt(data))
+    file = open("./sub.enc." + name, "wb")
+    file.write(sub_ciph.encrypt(data))
+    file.close()
 
     print("Decrypt :", sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map))
-    open(name+".sub.dec", "w").write(sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map))
+    file = open("./sub.dec." + name, "wb")
+    file.write(sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map))
+    file.close()
 
     print("Key =", sub_ciph.key_map)
-    open(name+".sub.key", "w").write(str(sub_ciph.key_map))
-
-    if data == sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map):
-        print("Data == Decrypt")
-    #print("ABC =", sub_ciph.abc)
+    file = open("./sub.key." + name, "w")
+    file.write(str(sub_ciph.key_map))
+    file.close()
 
 
+def Test_Transposition(name, data):
     ###########################################
     #                                         #
     #          TRANSPOSITION CIPHER           #
@@ -142,16 +145,22 @@ def main():
     tr_ciph = Transposition_cipher()
     print("\nData    :", data)
     print("Encrypt :", tr_ciph.encrypt(data))
-    open(name+".tr.enc", "w").write(tr_ciph.encrypt(data))
+    file = open("./tr.enc." + name, "wb")
+    file.write(tr_ciph.encrypt(data))
+    file.close()
 
     print("Decrypt :", tr_ciph.decrypt(tr_ciph.encrypt(data), tr_ciph.key_map))
-    open(name+".tr.dec", "w").write(tr_ciph.decrypt(tr_ciph.encrypt(data), tr_ciph.key_map))
+    file = open("./tr.dec." + name, "wb")
+    file.write(tr_ciph.decrypt(tr_ciph.encrypt(data), tr_ciph.key_map))
+    file.close()
 
     print("Key =", tr_ciph.key_map)
-    open(name+".tr.key", "w").write(str(tr_ciph.key_map))
-    #if data == sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map):
-    #    print("Data == Decrypt")
+    file = open("./tr.key." + name, "w")
+    file.write(str(tr_ciph.key_map))
+    file.close()
 
+
+def Test_Vigener(name, data):
     ###########################################
     #                                         #
     #             VIGENERE CIPHER             #
@@ -161,10 +170,29 @@ def main():
     vz_ciph = Vigenere_cipher()
     print("\nData    :", data)
     print("Encrypt :", vz_ciph.encrypt(data))
+    file = open("./vz.enc." + name, "wb")
+    file.write(vz_ciph.encrypt(data))
+    file.close()
+
     print("Decrypt :", vz_ciph.decrypt(vz_ciph.encrypt(data), vz_ciph.key_string))
+    file = open("./vz.dec." + name, "wb")
+    file.write(vz_ciph.decrypt(vz_ciph.encrypt(data), vz_ciph.key_string))
+    file.close()
+
     print("Key =", vz_ciph.key_string)
-    #if data == sub_ciph.decrypt(sub_ciph.encrypt(data), sub_ciph.key_map):
-    #    print("Data == Decrypt")
+    file = open("./vz.key." + name, "wb")
+    file.write(vz_ciph.key_string)
+    file.close()
+
+
+def main():
+    name = input("Input path file: ")
+    if name == "":
+        name = "qe.xlsx"
+    data = open("./" + name, "rb").read()
+    Test_Substitution(name, data)
+    Test_Transposition(name, data)
+    Test_Vigener(name, data)
 
 
 main()
